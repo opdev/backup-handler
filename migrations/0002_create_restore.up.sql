7183f51ff18c112dac60fb22af20787222864b7f
Edmund Ochieng <ochienged@gmail.com>
2022-06-07 11:33:25 +0300

    fix backup timestamps
    Store timestamps to sqlite3 as string as workaround for malformed times being returned when sqlite datetime is used
    
    Signed-off-by: Edmund Ochieng <ochienged@gmail.com>

--- migrations/0002_create_restore.up.sql
+++ migrations/0002_create_restore.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE restores (
-    created_at datetime,
-    updated_at datetime,
-    deleted_at datetime,
+    created_at varchar(32),
+    updated_at varchar(32),
+    deleted_at varchar(32),
     id varchar(72),
     name varchar(72),
     namespace varchar(120),
